47ede3b103e5e0abe04b8042ee66be25853c202b
vedanti <vedanti.r.bagade@gmail.com>
2025-06-28 21:02:37 +0530

    file updated

--- functions.php
+++ functions.php
@@ -2,7 +2,9 @@
 
 function coastora_register_styles()
 {
-    wp_enqueue_style('coastora-bootstrap', get_template_directory_uri() . "/style.css", array(), '1.0', 'all');
+    wp_enqueue_style('coastora-style', get_template_directory_uri() . "/style.css", array(), '1.0', 'all');
+    wp_enqueue_style('coastora-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '1.0', 'all');
+    wp_enqueue_style('coastora-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '1.0', 'all');
 }
 
 add_action('wp_enqueue_scripts', 'coastora_register_styles');
