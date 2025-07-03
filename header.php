7c1597a27af3ab280e0e74d4f2928dc56e516506
vedanti <vedanti.r.bagade@gmail.com>
2025-07-03 16:59:50 +0530

    file updated

--- header.php
+++ header.php
@@ -31,7 +31,8 @@
             <div id="navigation" class="collapse navbar-collapse flex-column">
                 <?php
                 if (function_exists('the_custom_logo')) {
-                    the_custom_logo();
+                    $custom_logo_id = get_theme_mod('custom_logo');
+                    $logo = wp_get_attachment_image_src($custom_logo_id);
                 }
                 ?>
                 <img class="mb-3 mx-auto logo" src="images/logo.png" alt="logo">
