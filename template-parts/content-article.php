be9883e81b2028537259d2c7cf6d4ed4ef980ad8
vedanti <vedanti.r.bagade@gmail.com>
2025-07-04 16:48:31 +0530

    file updated

--- template-parts/content-article.php
+++ template-parts/content-article.php
@@ -3,9 +3,8 @@
         <div class="meta mb-3">
             <span class="date"><?php the_date(); ?></span>
             <?php
-            the_tags('<span class="tag"><i class="fa fa-tag"></i>', '</span><span class="tag"><i class="fa fa-tag"></i>', '</span>')
+            the_tags('<span class="tag"><i class="fa fa-tag"></i> ', '</span><span class="tag"><i class="fa fa-tag"></i> ', '</span>')
                 ?>
-            <span class="tag"><i class='fa fa-tag'></i>tag</span>
             <span class="tag"><i class='fa fa-tag'></i>category</span>
             <span class="comment"><a href="#comments"><i class='fa fa-comment'></i> 3 comments</a></span>
         </div>
