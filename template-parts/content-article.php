d530e38e4cb2264b5f37b09063a8828044ce93c0
vedanti <vedanti.r.bagade@gmail.com>
2025-07-04 16:47:12 +0530

    file updated

--- template-parts/content-article.php
+++ template-parts/content-article.php
@@ -1,3 +1,17 @@
-<?php
-the_content();
-?>
+<div class="container">
+    <header class="content-header">
+        <div class="meta mb-3">
+            <span class="date"><?php the_date(); ?></span>
+            <?php
+            the_tags('<span class="tag"><i class="fa fa-tag"></i>', '</span><span class="tag"><i class="fa fa-tag"></i>', '</span>')
+                ?>
+            <span class="tag"><i class='fa fa-tag'></i>tag</span>
+            <span class="tag"><i class='fa fa-tag'></i>category</span>
+            <span class="comment"><a href="#comments"><i class='fa fa-comment'></i> 3 comments</a></span>
+        </div>
+    </header>
+
+    <?php
+    the_content();
+    ?>
+</div>
