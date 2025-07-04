22855cf915bf8e7464f57e6d8618e26ff6d68521
vedanti <vedanti.r.bagade@gmail.com>
2025-07-04 16:20:29 +0530

    file updated

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@ function coastora_theme_support()
     // Adds dynamic title tag support
     add_theme_support('title-tag');
     add_theme_support('custom-logo');
+    add_theme_support('post-thumbnails');
 }
 
 add_action('after_setup_theme', 'coastora_theme_support');
