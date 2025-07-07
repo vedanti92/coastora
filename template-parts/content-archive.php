e4db060d60c2ce1177a01b47d18212791150398e
vedanti <vedanti.r.bagade@gmail.com>
2025-07-07 18:29:17 +0530

    file updated

--- template-parts/content-archive.php
+++ template-parts/content-archive.php
@@ -1,7 +1,8 @@
 <div class="container">
     <div class="post mb-5">
         <div class="media">
-            <img class="mr-3 img-fluid post-thumb d-none d-md-flex" src="images/thumbs/1.jpg" alt="image">
+            <img class="mr-3 img-fluid post-thumb d-none d-md-flex" src="<?php the_post_thumbnail_url(); ?>"
+                alt="image">
             <div class="media-body">
                 <h3 class="title mb-1"><?php the_title(); ?></h3>
                 <div class="meta mb-1">
