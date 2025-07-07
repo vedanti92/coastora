191b4d356f5074e4dbc8931d3be004a724a3190e
vedanti <vedanti.r.bagade@gmail.com>
2025-07-07 18:15:46 +0530

    file updated

--- template-parts/content-archive.php
+++ template-parts/content-archive.php
@@ -6,15 +6,13 @@
                 <h3 class="title mb-1"><a href="post.html">Why Every Developer Should Have A Blog</a></h3>
                 <div class="meta mb-1"><span class="date">Published 2 days ago</span><span class="time">5 min
                         read</span><span class="comment"><a href="#">8 comments</a></span></div>
-                <div class="intro">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget
-                    dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur
-                    ridiculus mus. Donec quam felis, ultricies...</div>
+                <div class="intro">
+                    <?php
+                    the_excerpt();
+                    ?>
+                </div>
                 <a class="more-link" href="post.html">Read more &rarr;</a>
             </div><!--//media-body-->
         </div><!--//media-->
     </div>
-
-    <?php
-    the_excerpt();
-    ?>
 </div>
