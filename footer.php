b02f151ced92f9309f0b7f9d44cb7775544f98cc
vedanti <vedanti.r.bagade@gmail.com>
2025-07-08 13:00:41 +0530

    file updated

--- footer.php
+++ footer.php
@@ -1,6 +1,9 @@
 <footer class="footer text-center py-2 theme-bg-dark">
 
-    <p class="copyright"><a href="https://youtube.com/FollowAndrew">FollowAndrew</a></p>
+    <p class="copyright"><a href="#">Coastora</a></p>
+    <?php
+    dynamic_sidebar('footer-1');
+    ?>
 
 </footer>
 
