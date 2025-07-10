4a2d5c4064d6890f892f411e607212c7399b126f
vedanti <vedanti.r.bagade@gmail.com>
2025-07-10 19:49:44 +0530

    file updated

--- header.php
+++ header.php
@@ -35,7 +35,7 @@
                     $logo = wp_get_attachment_image_src($custom_logo_id);
                 }
                 ?>
-                <img class="mb-3 mx-auto logo" src="<?php echo $logo[0] ?>" alt="logo">
+                <img class="my-3 mx-auto logo" src="<?php echo $logo[0] ?>" alt="logo">
 
                 <?php
                 wp_nav_menu(
